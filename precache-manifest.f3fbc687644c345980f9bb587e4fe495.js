41acb4664838cecc924c5803bacc417b718ff3da
sunhurts <sunhurts@immutable.works>
2020-06-10 09:18:26 +0530

    updated to use build files

---
+++ precache-manifest.f3fbc687644c345980f9bb587e4fe495.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5848b56f38d1c289884b04007779a60",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dac4c79c852091c5d43d",
+    "url": "/static/css/main.c61c320c.chunk.css"
+  },
+  {
+    "revision": "9e34454d881d07d5776a",
+    "url": "/static/js/2.d83f7e54.chunk.js"
+  },
+  {
+    "revision": "62c5859e406d8a9f43e5a782482b23ea",
+    "url": "/static/js/2.d83f7e54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dac4c79c852091c5d43d",
+    "url": "/static/js/main.07c94f9b.chunk.js"
+  },
+  {
+    "revision": "f928b3db0a06278f8241",
+    "url": "/static/js/runtime-main.c1a10eab.js"
+  },
+  {
+    "revision": "ce61a549bea515fb5d731af251578e6e",
+    "url": "/static/media/arrow.ce61a549.svg"
+  },
+  {
+    "revision": "c0906d80ef3191834cc878cf7777b04a",
+    "url": "/static/media/btclogo.c0906d80.svg"
+  },
+  {
+    "revision": "aba8b6a0bb532a3781314b5f4515081b",
+    "url": "/static/media/btcswaplogo.aba8b6a0.png"
+  },
+  {
+    "revision": "a989e76065bb109282436e2e6f9da0ab",
+    "url": "/static/media/githublogo.a989e760.svg"
+  },
+  {
+    "revision": "24e988490ffd24e89fe2801f6772ba51",
+    "url": "/static/media/gmaillogo.24e98849.svg"
+  },
+  {
+    "revision": "b95f1e78a100f6ad80e38d05fa75285f",
+    "url": "/static/media/multibtclogo.b95f1e78.svg"
+  },
+  {
+    "revision": "15d4c6ba00536fbb9bd78c2a52967863",
+    "url": "/static/media/suisseintl-bold-webfont.15d4c6ba.woff"
+  },
+  {
+    "revision": "918d62e2213e7e15b244519b3cb0af31",
+    "url": "/static/media/suisseintl-bold-webfont.918d62e2.woff2"
+  },
+  {
+    "revision": "6c13e7d8ccde098221c136e286702708",
+    "url": "/static/media/suisseintl-bolditalic-webfont.6c13e7d8.woff"
+  },
+  {
+    "revision": "e16d899c0e9f49f8bd579073cb137c2b",
+    "url": "/static/media/suisseintl-bolditalic-webfont.e16d899c.woff2"
+  },
+  {
+    "revision": "1a33dfa96eaa3b2e4afeadaad3cc0704",
+    "url": "/static/media/suisseintl-light-webfont.1a33dfa9.woff"
+  },
+  {
+    "revision": "e55b8c1abe29b4df3860175a1d091a45",
+    "url": "/static/media/suisseintl-light-webfont.e55b8c1a.woff2"
+  },
+  {
+    "revision": "743c1f11926e9f591abcdf3db223374e",
+    "url": "/static/media/suisseintl-lightitalic-webfont.743c1f11.woff2"
+  },
+  {
+    "revision": "e6e18165b2bb80742c5e933671b061e1",
+    "url": "/static/media/suisseintl-lightitalic-webfont.e6e18165.woff"
+  },
+  {
+    "revision": "512fb1df4e8533699dc9bbf091d58057",
+    "url": "/static/media/suisseintl-medium-webfont.512fb1df.woff2"
+  },
+  {
+    "revision": "ffc971da9477712a7bc4a449bb598d6b",
+    "url": "/static/media/suisseintl-medium-webfont.ffc971da.woff"
+  },
+  {
+    "revision": "43b46a4cad012523dcda8f4b64584658",
+    "url": "/static/media/suisseintl-mediumitalic-webfont.43b46a4c.woff2"
+  },
+  {
+    "revision": "5bb30446f04d61419f7937b356329b75",
+    "url": "/static/media/suisseintl-mediumitalic-webfont.5bb30446.woff"
+  },
+  {
+    "revision": "1495de4f1a7e61d96cc57ef0e8f9685e",
+    "url": "/static/media/suisseintl-regular-webfont.1495de4f.woff2"
+  },
+  {
+    "revision": "241ec7b2709c077dd293af36ed73aeb6",
+    "url": "/static/media/suisseintl-regular-webfont.241ec7b2.woff"
+  },
+  {
+    "revision": "63c73e7c9aa0bea388c6e1e82800386e",
+    "url": "/static/media/suisseintl-regularitalic-webfont.63c73e7c.woff"
+  },
+  {
+    "revision": "90659012b21d2e8d564f9d171c70923a",
+    "url": "/static/media/suisseintl-regularitalic-webfont.90659012.woff2"
+  },
+  {
+    "revision": "f142a9559364a64bd7915a2bfeb17129",
+    "url": "/static/media/wbtclogo.f142a955.svg"
+  }
+]);
